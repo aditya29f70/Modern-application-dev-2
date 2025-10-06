99b64a5fc80dac4649369307a2097dba01615617
aditya <adityakumar29f70@gmail.com>
2025-10-06 18:49:19 +0530

    learnt about js normal fn and arrow fn , how 'this'  context change when we declare a fn , also inside the obj . and about arrow that 'this' obj will be same that 'this' obj that was above outside of that arrow fn

--- app.js
+++ app.js
@@ -121,32 +121,227 @@ var c= 10 ; //creates variable and property in browser mean
 // eg bz note; a= function(){} <-- this is expression
 // also called  anonamus fn
 
-let a= function(){}
 
-//  called it like --> a(); lly in both
-a();
+// console.log(a());
+
+// let a= function(){
+//   console.log('a');
+// }
+
+// //  called it like --> a(); lly in both
+// a();
 
 
-// 2. fn declaration (normal declaration of fn)
+// // 2. fn declaration (normal declaration of fn)
 
-function b(){
 
-}
+// console.log(b());
 
-b();
+// function b(){
+//   console.log('b');
+// }
+
+// b();
 
 
 // diff come in hosting
+//  so fn expression is hoisted based on the rules of variable declaration -> you can't access them before declaration
+// so if you declared it by using let and const it will be in TDZ so before assign any value to it if you ask value of that -> will give error
+
+//  and fn declaration is fully hoisted and fully usable
+
+
+// note if in you fn expression is not returning anything and you asking a() ; i mean ; eg clg(a()) --> lly to undefined() --> give error bz if you haven't returning anything
+
+
+// ######################### lets move to the arrow fn
+
+//  arrow fn are alway fn expression --> like let talk = () => {}
+// since they don't have name so there name is always fn expression or fn declaraion
+// 
+
+// let talk= () => {
+//   console.log('talking');
+// }
+
+
+// talk();
+
+// what are the differances bw arrow and regular fns
+// arrow fn should not be used as methods
+
+
+// we have seen how to declare method (there two way to declare see)
+
+// const person= {
+//   'f-name': 'Aditya',
+//   talk: function(){
+//     console.log(`${this['f-name']} talks too much!`);
+//   }, // first way to write method
+//   talk2(){}, //second way to write method
+
+//   //there is also other method to define method --> through arrow fn
+//   arrowTalk: ()=> {
+//     //if i do same thing here this will create problem
+//     //bz here 'this' keyword doesn't refer to person obj -> instead refer to the global obj
+//     console.log(`${this['f-name']} talks too much!`);
+
+//     // so that is diff that arrow fn and normal fn behave deff in the obj
+    
+//   }
+// }
+// person.talk()
+// person.arrowTalk()
+
+// now see how it ('this') is excutting here for both senorio
+
+//###### normal fn 
+
+// what is this ?? how it is giving the value
+// question yourself what is on your left handside of the fn who is calling this fn 
+// eg
+// abc(); //--> here nothing on the left side means --> window obj is called it or globalThis 
+//or (this -->  would be global obj) or global obj is calling it
+ 
+
+
+// function abc(){
+//   console.log(this);
+// }
+
+// abc()
+
+
+// // if this attached to an obj
+// // so obj would be on there left side --> so that obj itself -> this
+// // eg
+// obj = {
+//   a : 'rishav',
+//   xyz(){
+//     console.log(this);
+//   }
+// }
+
+
+// obj.xyz(); // what is left of xyz 'obj' so only inside the xyz , this obj would we --> 'this'
+// not this only for fn mean inside a fn we be calling 'this' then this will be that obj object
+// other wise if we normally asking 'this' so this will be global this
+
+
+// note; const object can be modefy 'const' doesn't mean obj doesn't modefy here, you can say obj = somethingelse
+
+// now see obj inside obj
+
+
+// const obj3= {
+//   b: 'aditya',
+//   xyz(){
+//     console.log(this);
+//   },
+//   obj4: {
+//     c: 'ram',
+//     xyy(){
+//       console.log(this);
+//     }
+//   }
+// }
+
+// obj3.obj4.xyy()
 
 
 
-// >>>>>>>>>>>> time; 1:56:31
+// now we can also assign a outer fn to value of key inside a obj
+
+// function abc(){
+//   console.log(this);
+// }
+
+// abc(); //would be window fn 
+
+
+// const obj5={
+//   meth1: abc
+// }
+
+// obj5.meth1() //now 'this' obj inside a fn which use obj5 obj so this 'this' obj will be -> obj5
+
+
+
+// ### arrow fn (this topic)
+
+
+// let arrowFunc= () => {
+//   console.log(this);
+// }
+
+// arrowFunc();
+
+// //this arrow fn not in any obj so 'this' obj will be -> window obj --> when we will call this fn
+// // what exactly happens in arrow fn is -> when you define this fn what is outside this arrow fn is fixed
+// //  
+
+
+
+// const ob= {
+//   ob_name: 'rishav',
+//   arrowfunc: ()=> {
+//     console.log(this);
+//   }
+// }
+
+// ob.arrowfunc();
+
+
+// const ob1= {
+//   ob_name: 'rishav',
+//   ob2:{
+//     ob2_name: 'aditya',
+//     arrowfun:()=> {
+//       console.log(this);  //note; 'this' is fixed at creation; and what is value of this 'this' -> what ever outside it -> the whole window obj
+//     }
+//   }
+// }
+
+// ob1.ob2.arrowfun();
+
+//
 
 
 
 
+// ########## at the end remember only is a 'this' is used in a fn and that fn inside an obj then that obj would we 'this' obj (for normal fn )
+// and for this alway try to see left hand side -> if we are taking obj to call that fn then that obj would be 'this'
+//eg  (note; see note when every you define new normal fn --> new 'this' will be created)
+// const o= {
+//   o_name: 'obj',
+//   f: function(){
+//     let normal= function(){
+//       console.log(this)
+//     }
+//     normal() //normal is normal fn and here we don't need to call any obj for this fn so 'this'--> window fn 
+//   }
+// }
+
+// o.f()
 
 
+
+// // ############ if 'this' obj in side a arrow fn and want to know what is 'this' obj so -> just go one outside and ask what 'this' contain if 'this' cantain 'window' obj then it will be window or if any obj then would be that obj
+// // eg
+
+// const objec={
+//   obj_name: 'rishav',
+//   fn: function(){
+//     //let def a arrow fn 
+//     // outside arrow fn 'this'= > objec
+//     let arrow_fn = ()=> {
+//       console.log(this);
+//     }
+
+//     arrow_fn()
+//   }
+// }
+// objec.fn()
 
 
 
