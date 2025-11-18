cf7f35de43539acff7ea0766f10e0774ca420537
Aditya Kumar <adityakumar29f70@gmail.com>
2025-11-18 11:47:43 +0530

    week 6

--- 6.js
+++ 6.js
@@ -1,3 +1,5 @@
 // we will try to understand how do you creat pages or structure so you can navigate one page to other
+// and this will basically done with help of view-router
 
-    
+// so vue router is basically is extension in view frame work therefore it is not 
+
