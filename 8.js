97f0cf9c8120eaa4222a7fdf12d4413acee58356
Aditya Kumar <adityakumar29f70@gmail.com>
2025-12-01 02:17:39 +0530

    week 8

--- 8.js
+++ 8.js
@@ -1,1 +1,4 @@
 // this week is more about backent we will be seeing more about security
+// so we also need to protect our routes;; and that can be done in multiple ways
+
+// working on flask jwt (json web token) will used to provide security using token
