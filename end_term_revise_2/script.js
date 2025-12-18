4a1adeca8091b1c85eddd8487200b27c11e6b6d0
Aditya Kumar <adityakumar29f70@gmail.com>
2025-12-19 02:12:37 +0530

    learning for end term

--- end_term_revise_2/script.js
+++ end_term_revise_2/script.js
@@ -23,13 +23,19 @@ const app = new Vue({
   data:{
     newData:'',
     todoList: new Array(),
+    myTodos:""
+  },
+  mounted(){
+    this.myTodos= localStorage.getItem('todolist')
   },
   methods:{
     addTodoList(){
       this.todoList.push(this.newData)
-      localStorage.setItem(STORAGE_KEY.JSON.stringify(this.newData))
+      // if you want to add something in localStorage so the way to store --> localStorage.setItem(key(key_of_local_storage_where_you_want_to_store_that_value), value(that value which you wanted to store))
+      localStorage.setItem('todolist', this.todoList) // it will not store as array of element it will store string of that elements
+      // localStorage.setItem(STORAGE_KEY.JSON.stringify(this.todoList))
     }
   }
 })
 
-
+1:20
