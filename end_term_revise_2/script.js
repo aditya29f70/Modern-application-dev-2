f4b61fdd618890527109ec12b1858a607d3743d3
aditya <adityakumar29f70@gmail.com>
2025-12-19 20:33:14 +0530

    end term prepartion

--- end_term_revise_2/script.js
+++ end_term_revise_2/script.js
@@ -26,16 +26,47 @@ const app = new Vue({
     myTodos:""
   },
   mounted(){
-    this.myTodos= localStorage.getItem('todolist')
+    // this.myTodos= localStorage.getItem('todolist')
+    this.todoList= JSON.parse(localStorage.getItem('todolist'))
   },
   methods:{
     addTodoList(){
       this.todoList.push(this.newData)
       // if you want to add something in localStorage so the way to store --> localStorage.setItem(key(key_of_local_storage_where_you_want_to_store_that_value), value(that value which you wanted to store))
-      localStorage.setItem('todolist', this.todoList) // it will not store as array of element it will store string of that elements
+      localStorage.setItem('todolist', JSON.stringify(this.todoList)) // it will not store as array of element it will store string of that elements
       // localStorage.setItem(STORAGE_KEY.JSON.stringify(this.todoList))
     }
   }
 })
 
-1:20
+//note:: when we add value convert it in the stringify in the local stoage now it is saving things as stringify array, before it is saving string of things which is inside toDoList
+// localStorage store things only in string format
+
+// our array was --> ['todo1', 'todo2']
+//  --> "todo1","todo2" -- w/0 stringify
+//  --> "['todo1', 'todo2']" -- with stringify
+// --> ["todo1", "todo2"] -- with JSON.parser
+
+
+// // here background-color-> in cabab case
+// <div style='background-color: red'></div>
+
+// // here backgroundColor in Camel case
+// <div v-bind:style='red'></div>
+
+// red: {
+//   backgroundColor:red;
+// }
+
+
+// Before_created  // can't access data
+//      |
+// Created // can access data
+//      |
+// ################ Component
+// before_mounted // can access data
+//      |
+// mounted //can access data
+
+// before update (want to show loader)
+// updated
